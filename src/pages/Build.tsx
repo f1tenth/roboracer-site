67a765b69de441416cca1c8ed3472a53a1736420
Shreyas Raorane <raorane@seas.upenn.edu>
2025-12-24 18:48:19 +0530

    top margin added

--- src/pages/Build.tsx
+++ src/pages/Build.tsx
@@ -1,6 +1,6 @@
 export default function Build() {
     return (
-      <div className="w-full h-[100svh]">
+      <div className="w-full h-[100svh] pt-[68px] md:pt-[85px]">
         <iframe
           className="w-full h-full border-none"
           src="https://f1tenth.readthedocs.io/en/foxy_test/"
