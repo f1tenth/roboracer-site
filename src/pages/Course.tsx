5a0d882151ed535099f3ef3123be7f311bd156f6
Ahmad Amine <ahmadamine998@gmail.com>
2026-03-11 03:33:32 -0400

    Fix course website url

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -3,7 +3,7 @@ export default function Course() {
       <div className="w-full h-[100svh] pt-[68px] md:pt-[85px]">
         <iframe
           className="w-full h-full border-none"
-          src="https://ahmadamine998.github.io/6150-Website"
+          src="https://ahmadamine998.github.io/ESE6150-Website/"
           title="6150 Spring 2024 Course Website"
         />
       </div>
